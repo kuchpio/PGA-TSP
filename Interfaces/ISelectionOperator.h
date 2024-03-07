1727306d3607ad84c0b27d60cf7205a128d029f3
bmajosek <bartek9568@gmail.com>
2024-03-07 19:08:20 +0100

    repair selection operator

--- Interfaces/ISelectionOperator.h
+++ Interfaces/ISelectionOperator.h
@@ -5,7 +5,7 @@
 class ISelectionOperator
 {
 public:
-	virtual void selection(int** population, int* chromosome, int size, int populationSize, curandState* state) const = 0; // int** global memory :/
+	virtual void selection(int* drawnChromosome, int* chromosome, int size, int populationSize, curandState* state) const = 0;
 	float calculateFitness(int* chromosome, int size) const {
 		return 0;
 	} // add all edges from tex2D i->j size + 1 edges
