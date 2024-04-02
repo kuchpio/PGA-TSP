4943b124e338f97dc9d6fa3c836543e85793085a
Bartosz Maj <bartek9568@gmail.com>
2024-04-02 16:40:41 +0200

    remove unnecessary args

--- Kernel/Kernel.cuh
+++ Kernel/Kernel.cuh
@@ -4,4 +4,4 @@ void shuffleChromosome(int* chromosome, int size, curandState* state);
 
 void geneticAlgorithmKernel(int** population, float** distance_matrix, int size, curandState* globalState, int max_iterations);
 
-void tspGeneticAlgorithmKernel(int** population, float** distance_matrix, float* fitness, int size, int populationSize, curandState* globalState, int max_iterations);
+void tspGeneticAlgorithmKernel(int** population, float** distance_matrix, int size, curandState* globalState, int max_iterations);
