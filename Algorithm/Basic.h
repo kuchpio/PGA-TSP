ff3d2dcc94d0adb1f9cb694a9f58406e917e4a07
bmajosek <bartek9568@gmail.com>
2024-04-09 21:29:34 +0200

    update geneticAlgorithmKernel function

--- Algorithm/Basic.h
+++ Algorithm/Basic.h
@@ -12,27 +12,26 @@
 #include "../Mutations/Basic.h"
 
 namespace tsp {
-
 	template <typename Instance>
 	__global__
-	void solveTSPBasicKernel(const Instance instance, int *fitness, int *population, curandState* globalState, int maxIterations) {
+		void solveTSPBasicKernel(const Instance instance, int* fitness, int* population, curandState* globalState, int maxIterations) {
 		int tid = blockIdx.x * blockDim.x + threadIdx.x;
 		int instanceSize = size(instance);
 		curandState localState = globalState[tid];
-		int *chromosome = population + tid * instanceSize;
-		int *result = new int[instanceSize];
+		int* chromosome = population + tid * instanceSize;
+		int* result = new int[instanceSize];
 
 		initChromosome(chromosome, instanceSize, &localState);
 		fitness[tid] = hamiltonianCycleWeight(instance, chromosome);
-		
+
 		for (int i = 0; i < maxIterations; i++) {
 			// Selection
 			__syncthreads();
 			int selectionCandidateTid = curand(&localState) % blockDim.x + blockIdx.x * blockDim.x;
-			select(chromosome, population + selectionCandidateTid * instanceSize, result, 
+			select(chromosome, population + selectionCandidateTid * instanceSize, result,
 				instanceSize, fitness[tid], fitness[selectionCandidateTid]);
 			__syncthreads();
-			for (int i = 0; i < instanceSize; i++) 
+			for (int i = 0; i < instanceSize; i++)
 				chromosome[i] = result[i];
 
 			// Crossover
@@ -40,7 +39,7 @@ namespace tsp {
 			int crossoverParentTid = curand(&localState) % blockDim.x + blockIdx.x * blockDim.x;
 			crossover(chromosome, population + crossoverParentTid * instanceSize, result, instanceSize, &localState);
 			__syncthreads();
-			for (int i = 0; i < instanceSize; i++) 
+			for (int i = 0; i < instanceSize; i++)
 				chromosome[i] = result[i];
 
 			// Mutation
@@ -49,14 +48,14 @@ namespace tsp {
 			// Fitness
 			fitness[tid] = hamiltonianCycleWeight(instance, chromosome);
 		}
-		
+
 		delete[] result;
 	}
 
 	template <typename Instance>
 	int solveTSP(const Instance instance) {
 		const int blockSize = 256, gridSize = 4;
-		int* d_fitness, *d_population;
+		int* d_fitness, * d_population;
 		curandState* d_globalState;
 
 		if (cudaMalloc(&d_fitness, blockSize * gridSize * sizeof(int)) != cudaSuccess)
@@ -68,12 +67,12 @@ namespace tsp {
 		if (cudaMalloc(&d_globalState, blockSize * gridSize * sizeof(curandState)) != cudaSuccess)
 			return -1;
 
-		setupCurand<<<gridSize, blockSize>>>(d_globalState, 100);
+		setupCurand << <gridSize, blockSize >> > (d_globalState, 100);
 
 		if (cudaDeviceSynchronize() != cudaSuccess)
 			return -1;
 
-		solveTSPBasicKernel<<<gridSize, blockSize>>>(instance, d_fitness, d_population, d_globalState, 1000);
+		solveTSPBasicKernel << <gridSize, blockSize >> > (instance, d_fitness, d_population, d_globalState, 1000);
 
 		if (cudaDeviceSynchronize() != cudaSuccess)
 			return -1;
@@ -84,10 +83,43 @@ namespace tsp {
 		cudaFree(d_fitness);
 		cudaFree(d_population);
 		cudaFree(d_globalState);
-		
+
 		return opt;
 	}
 
+	template <typename Instance>
+	int solveTSP2(const Instance instance) {
+		const int blockSize = 256, gridSize = 4;
+		int* d_fitness, * d_population;
+		curandState* d_globalState;
+
+		if (cudaMalloc(&d_fitness, blockSize * gridSize * sizeof(int)) != cudaSuccess)
+			return -1;
+
+		if (cudaMalloc(&d_population, blockSize * gridSize * size(instance) * sizeof(int)) != cudaSuccess)
+			return -1;
+
+		if (cudaMalloc(&d_globalState, blockSize * gridSize * sizeof(curandState)) != cudaSuccess)
+			return -1;
+
+		setupCurand << <gridSize, blockSize >> > (d_globalState, time(NULL));
+
+		if (cudaDeviceSynchronize() != cudaSuccess)
+			return -1;
+
+		geneticAlgorithmKernel << <gridSize, blockSize >> > (instance, d_fitness, d_population, d_globalState, 1000);
+
+		if (cudaDeviceSynchronize() != cudaSuccess)
+			return -1;
+
+		auto fitnessPtr = thrust::device_pointer_cast(d_fitness);
+		int opt = *thrust::min_element(fitnessPtr, fitnessPtr + blockSize * gridSize);
+		cudaFree(d_fitness);
+		cudaFree(d_population);
+		cudaFree(d_globalState);
+
+		return opt;
+	}
 }
 
-#endif 
+#endif
