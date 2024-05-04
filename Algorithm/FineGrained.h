5a43b2bc1ac80734608b8784860515f0bcf9c79a
kuchpio <53230073+kuchpio@users.noreply.github.com>
2024-05-04 17:02:49 +0200

    Refactoring, template gene type

--- Algorithm/FineGrained.h
+++ Algorithm/FineGrained.h
@@ -10,12 +10,22 @@
 
 namespace tsp {
 
-	__device__ __forceinline__ void initializeChromosome(int* chromosome, unsigned int n, curandState *state) {
-		unsigned int lid = (blockDim.x * blockIdx.x + threadIdx.x) & (WARP_SIZE - 1);
+	struct IslandGeneticAlgorithmOptions {
+		unsigned int islandCount;
+		unsigned int islandPopulationSize;
+		unsigned int isolatedIterationCount;
+		unsigned int migrationCount;
+		float crossoverProbability;
+		float mutationProbability;
+		bool elitism;
+	};
 
-		for (unsigned int i = 0; i < n; i += WARP_SIZE) {
-			chromosome[i + lid] = i + lid;
-		}
+	template <typename vertex>
+	__device__ __forceinline__ void initializeCycle(vertex* cycle, unsigned int n, curandState *state) {
+		vertex lid = (blockDim.x * blockIdx.x + threadIdx.x) & (WARP_SIZE - 1);
+
+		for (vertex i = lid; i < n; i += WARP_SIZE) 
+			cycle[i] = i;
 
 		// TODO: Parallelize shuffle
 		if (lid == 0) {
@@ -23,24 +33,24 @@ namespace tsp {
 				unsigned int j = curand(state) % (i + 1);
 
 				// Swap chromosome[i] with chromosome[j]
-				int temp = chromosome[i];
-				chromosome[i] = chromosome[j];
-				chromosome[j] = temp;
+				vertex temp = cycle[i];
+				cycle[i] = cycle[j];
+				cycle[j] = temp;
 			}
 		}
 	}
 
-	template <typename Instance>
-	__device__ __forceinline__ void calculateFitness(const int* chromosome, const Instance instance, int* fitness) {
+	template <typename Instance, typename vertex>
+	__device__ __forceinline__ int calculateCycleWeight(const vertex* cycle, const Instance instance) {
 		unsigned int lid = (blockDim.x * blockIdx.x + threadIdx.x) & (WARP_SIZE - 1);
 		unsigned int lidShfl = (lid + 1) & (WARP_SIZE - 1);
 		unsigned int n = size(instance);
 		unsigned int nWarpSizeAligned = (n & ~(WARP_SIZE - 1)) + WARP_SIZE;
 
 		int sum = 0;
-		int from = chromosome[lid];
-		int to = __shfl_sync(FULL_MASK, from, lidShfl);
-		int first, last;
+		vertex from = cycle[lid];
+		vertex to = __shfl_sync(FULL_MASK, from, lidShfl);
+		vertex first, last;
 
 		if (lid < WARP_SIZE - 1) {
 			sum += edgeWeight(instance, from, to);
@@ -50,13 +60,13 @@ namespace tsp {
 		}
 
 		for (unsigned int i = WARP_SIZE; i < nWarpSizeAligned - WARP_SIZE; i += WARP_SIZE) {
-			from = chromosome[i + lid];
+			from = cycle[i + lid];
 			to = __shfl_sync(FULL_MASK, from, lidShfl);
 			sum += edgeWeight(instance, lid == WARP_SIZE - 1 ? last : from, to);
 			if (lid == WARP_SIZE - 1) last = from;
 		}
 
-		from = chromosome[nWarpSizeAligned - WARP_SIZE + lid];
+		from = cycle[nWarpSizeAligned - WARP_SIZE + lid];
 		to = __shfl_sync(FULL_MASK, from, lidShfl);
 		if (nWarpSizeAligned - WARP_SIZE + lid < n - 1)
 			sum += edgeWeight(instance, from, to);
@@ -72,10 +82,10 @@ namespace tsp {
 		for (unsigned int i = 1; i < WARP_SIZE; i *= 2) 
 			sum += __shfl_xor_sync(FULL_MASK, sum, i);
 
-		if (lid == 0) *fitness = sum;
+		return sum;
 	}
 
-	__device__ __forceinline__ int findBestFitness(int current, int index) {
+	__device__ __forceinline__ int findMin(int current, int index) {
 		int currentShuf, indexShuf;
 		for (int i = 1; i < WARP_SIZE; i *= 2) {
 			currentShuf = __shfl_xor_sync(FULL_MASK, current, i);
@@ -88,7 +98,7 @@ namespace tsp {
 		return index;
 	}
 
-	__device__ __forceinline__ int findWorstFitness(int current, int index) {
+	__device__ __forceinline__ int findMax(int current, int index) {
 		int currentShuf, indexShuf;
 		for (int i = 1; i < WARP_SIZE; i *= 2) {
 			currentShuf = __shfl_xor_sync(FULL_MASK, current, i);
@@ -106,9 +116,9 @@ namespace tsp {
 		curand_init(seed, tid, 0, globalState + tid);
 	}
 
-	template <typename Instance>
-	__global__ void initializationKernel(const Instance instance, curandState* globalState, int* population, int *fitness, int *islandBest, int *islandWorst) {
-		__shared__ int s_fitness[WARP_SIZE];
+	template <typename Instance, typename gene>
+	__global__ void initializationKernel(const Instance instance, curandState* globalState, gene* population, int *cycleWeight, int *islandBest, int *islandWorst) {
+		__shared__ int s_cycleWeight[WARP_SIZE];
 		unsigned int tid = blockDim.x * blockIdx.x + threadIdx.x;	// Global thread id
 		unsigned int wid = tid >> 5;								// Global warp id
 		unsigned int blockWid = wid & (WARP_SIZE - 1);				// Block warp id
@@ -116,20 +126,21 @@ namespace tsp {
 		unsigned int lid = tid & (WARP_SIZE - 1);					// Warp thread id
 		unsigned int n = size(instance);
 		unsigned int nWarpSizeAligned = (n & ~(WARP_SIZE - 1)) + WARP_SIZE;
-		int *chromosome = population + wid * nWarpSizeAligned;
+		gene *chromosome = population + wid * nWarpSizeAligned;
 
-		initializeChromosome(chromosome, n, globalState + tid);
+		initializeCycle(chromosome, n, globalState + tid);
 
 		__syncthreads();
 
-		calculateFitness(chromosome, instance, s_fitness + blockWid);
+		int thisCycleWeight = calculateCycleWeight(chromosome, instance);
+		if (lid == 0) s_cycleWeight[blockWid] = thisCycleWeight;
 
 		__syncthreads();
 
 		if (blockWid == 0) {
-			int value = s_fitness[lid];
-			int bestLid = findBestFitness(value, lid);
-			int worstLid = findWorstFitness(value, lid);
+			int value = s_cycleWeight[lid];
+			int bestLid = findMin(value, lid);
+			int worstLid = findMax(value, lid);
 			if (lid == 0) {
 				if (bestLid == worstLid) {
 					islandBest[blockIdx.x] = 0;
@@ -140,10 +151,11 @@ namespace tsp {
 				}
 			}
 		}
-		if (blockWid == 1) fitness[baseWid + lid] = s_fitness[lid];
+		if (blockWid == 1) cycleWeight[baseWid + lid] = s_cycleWeight[lid];
 	}
 
-	__global__ void migrationKernel(int* population, int nWarpSizeAligned, int *fitness, int* islandBest, int* islandWorst) {
+	template <typename gene>
+	__global__ void migrationKernel(gene* population, int nWarpSizeAligned, int *cycleWeight, int* islandBest, int* islandWorst) {
 		__shared__ int s_widSrcDst[2];
 		if (threadIdx.x < WARP_SIZE) {
 			int islandPopulationSize = blockDim.x / WARP_SIZE;
@@ -153,20 +165,20 @@ namespace tsp {
 			int thisIslandWorstWidGlobal = thisIsland * islandPopulationSize + islandWorst[thisIsland];
 
 			// Correct fitness
-			int nextIslandBestFitness = fitness[nextIslandBestWidGlobal];
+			int nextIslandBestCycleWeight = cycleWeight[nextIslandBestWidGlobal];
 			if (threadIdx.x == 0)
-				fitness[thisIslandWorstWidGlobal] = nextIslandBestFitness;
+				cycleWeight[thisIslandWorstWidGlobal] = nextIslandBestCycleWeight;
 
 			__syncwarp();
 
-			int thisFitness = fitness[thisIsland * islandPopulationSize + threadIdx.x];
+			int thisCycleWeight = cycleWeight[thisIsland * islandPopulationSize + threadIdx.x];
 
 			// Correct islandBest
-			if (threadIdx.x == islandBest[thisIsland] && thisFitness > nextIslandBestFitness)
+			if (threadIdx.x == islandBest[thisIsland] && thisCycleWeight > nextIslandBestCycleWeight)
 				islandBest[thisIsland] = thisIslandWorstWidGlobal % islandPopulationSize;
 
 			// Correct islandWorst
-			int thisIslandWorstWidUpdated = findWorstFitness(thisFitness, threadIdx.x);
+			int thisIslandWorstWidUpdated = findMax(thisCycleWeight, threadIdx.x);
 
 			if (threadIdx.x == 0) {
 				s_widSrcDst[0] = nextIslandBestWidGlobal;
@@ -178,16 +190,16 @@ namespace tsp {
 		__syncthreads();
 
 		// Replace worst chromosome in thisIsland with best chromosome from nextIsland
-		int* srcChromosome = population + s_widSrcDst[0] * nWarpSizeAligned;
-		int* dstChromosome = population + s_widSrcDst[1] * nWarpSizeAligned;
+		gene* srcChromosome = population + s_widSrcDst[0] * nWarpSizeAligned;
+		gene* dstChromosome = population + s_widSrcDst[1] * nWarpSizeAligned;
 
 		for (unsigned int i = threadIdx.x; i < nWarpSizeAligned; i += blockDim.x)
 			dstChromosome[i] = srcChromosome[i];
 	}
 
-	template <typename Instance>
-	__global__ void islandEvolutionKernel(const Instance instance, curandState* globalState, unsigned int iterationCount, int* population, int* fitness, int *islandBest, int *islandWorst) {
-		__shared__ int s_fitness[WARP_SIZE];
+	template <typename Instance, typename gene>
+	__global__ void islandEvolutionKernel(const Instance instance, curandState* globalState, unsigned int iterationCount, gene* population, int* cycleWeight, int *islandBest, int *islandWorst) {
+		__shared__ int s_cycleWeight[WARP_SIZE];
 		__shared__ float s_roulletteWheelThreshold[WARP_SIZE];
 		unsigned int tid = blockDim.x * blockIdx.x + threadIdx.x;	// Global thread id
 		unsigned int wid = tid >> 5;								// Global warp id
@@ -195,9 +207,9 @@ namespace tsp {
 		unsigned int baseWid = wid & ~(WARP_SIZE - 1);				// Block base warp id
 		unsigned int lid = tid & (WARP_SIZE - 1);					// Warp thread id
 		unsigned int nWarpSizeAligned = (size(instance) & ~(WARP_SIZE - 1)) + WARP_SIZE;
-		int *chromosome = population + wid * nWarpSizeAligned;
+		gene *chromosome = population + wid * nWarpSizeAligned;
 
-		if (blockWid == 0) s_fitness[lid] = fitness[baseWid + lid];
+		if (blockWid == 0) s_cycleWeight[lid] = cycleWeight[baseWid + lid];
 
 		int islandBestWid, islandWorstWid;
 		if (blockWid == 0) {
@@ -213,9 +225,9 @@ namespace tsp {
 			{
 				if (blockWid == 0) {
 					// t(f) = (max - f) / (max - min)
-					int min = s_fitness[islandBestWid];
-					int max = s_fitness[islandWorstWid];
-					float intervalWidth = min == max ? 1.0f : ((float)(max - s_fitness[lid])) / ((float)(max - min));
+					int min = s_cycleWeight[islandBestWid];
+					int max = s_cycleWeight[islandWorstWid];
+					float intervalWidth = min == max ? 1.0f : ((float)(max - s_cycleWeight[lid])) / ((float)(max - min));
 					float intervalWidthShfl;
 
 					// Warp scan to get prefix sums
@@ -235,9 +247,9 @@ namespace tsp {
 				if (lid == WARP_SIZE - 1) rnd = curand_uniform(globalState + tid) * threshold;
 				rnd = __shfl_sync(FULL_MASK, rnd, WARP_SIZE - 1);
 				int selectedWid = WARP_SIZE - __clz(~(__ballot_sync(FULL_MASK, rnd <= threshold)));
-				int* selectedChromosome = population + (baseWid + selectedWid) * nWarpSizeAligned;
+				gene* selectedChromosome = population + (baseWid + selectedWid) * nWarpSizeAligned;
 				for (unsigned int i = lid; i < nWarpSizeAligned; i += WARP_SIZE) {
-					int buffer = selectedChromosome[i];
+					gene buffer = selectedChromosome[i];
 					__syncthreads();
 					chromosome[i] = buffer;
 				}
@@ -250,20 +262,23 @@ namespace tsp {
 			// Mutation
 
 			// Fitness
-			calculateFitness(chromosome, instance, s_fitness + blockWid);
+			{
+				int thisCycleWeight = calculateCycleWeight(chromosome, instance);
+				if (lid == 0) s_cycleWeight[blockWid] = thisCycleWeight;
+			}
 
 			__syncthreads();
 
 			// Best and Worst
 			if (blockWid == 0) {
-				int value = s_fitness[lid];
-				islandBestWid = findBestFitness(value, lid);
-				islandWorstWid = findWorstFitness(value, lid);
+				int thisCycleWeight = s_cycleWeight[lid];
+				islandBestWid = findMin(thisCycleWeight, lid);
+				islandWorstWid = findMax(thisCycleWeight, lid);
 			}
 		}
 
 		if (blockWid == 0) {
-			fitness[baseWid + lid] = s_fitness[lid];
+			cycleWeight[baseWid + lid] = s_cycleWeight[lid];
 			if (lid == 0) {
 				if (islandBestWid == islandWorstWid) {
 					islandBest[blockIdx.x] = 0;
@@ -276,91 +291,91 @@ namespace tsp {
 		}
 	}
 
-	template <typename Instance>
-	int solveTSPFineGrained(const Instance instance, unsigned int islandCount, unsigned int isolatedIterationCount, unsigned int migrationCount, int seed) {
-		unsigned int islandPopulationSize = 1024 / WARP_SIZE;
+	template <typename Instance, typename gene = unsigned short>
+	int solveTSPFineGrained(const Instance instance, struct IslandGeneticAlgorithmOptions options, int seed) {
 		unsigned int n = size(instance);
 		unsigned int nWarpSizeAligned = (n & ~(WARP_SIZE - 1)) + WARP_SIZE;
-		int *d_fitness, *d_population, *d_islandBest, *d_islandWorst;
+		int *d_cycleWeight, *d_islandBest, *d_islandWorst;
+		gene* d_population;
 		curandState* d_globalState;
-		int* h_fitness = new int[islandCount * islandPopulationSize];
-		int* h_islandBest = new int[islandCount];
-		int* h_islandWorst = new int[islandCount];
+		int* h_cycleWeight = new int[options.islandCount * options.islandPopulationSize];
+		int* h_islandBest = new int[options.islandCount];
+		int* h_islandWorst = new int[options.islandCount];
 
-		if (cudaMalloc(&d_fitness, islandCount * islandPopulationSize * sizeof(int)) != cudaSuccess)
+		if (cudaMalloc(&d_cycleWeight, options.islandCount * options.islandPopulationSize * sizeof(int)) != cudaSuccess)
 			return -1;
 
-		if (cudaMalloc(&d_population, nWarpSizeAligned * islandCount * islandPopulationSize * sizeof(int)) != cudaSuccess)
+		if (cudaMalloc(&d_population, nWarpSizeAligned * options.islandCount * options.islandPopulationSize * sizeof(gene)) != cudaSuccess)
 			return -1;
 
-		if (cudaMalloc(&d_globalState, islandCount * islandPopulationSize * WARP_SIZE * sizeof(curandState)) != cudaSuccess)
+		if (cudaMalloc(&d_globalState, options.islandCount * options.islandPopulationSize * WARP_SIZE * sizeof(curandState)) != cudaSuccess)
 			return -1;
 
-		if (cudaMalloc(&d_islandBest, islandCount * sizeof(int)) != cudaSuccess)
+		if (cudaMalloc(&d_islandBest, options.islandCount * sizeof(int)) != cudaSuccess)
 			return -1;
 
-		if (cudaMalloc(&d_islandWorst, islandCount * sizeof(int)) != cudaSuccess)
+		if (cudaMalloc(&d_islandWorst, options.islandCount * sizeof(int)) != cudaSuccess)
 			return -1;
 
-		setupCurand<<<islandCount, islandPopulationSize* WARP_SIZE>>>(d_globalState, seed);
+		setupCurand<<<options.islandCount, options.islandPopulationSize * WARP_SIZE>>>(d_globalState, seed);
 
-		initializationKernel<<<islandCount, islandPopulationSize * WARP_SIZE>>>(instance, d_globalState, d_population, d_fitness, d_islandBest, d_islandWorst);
+		initializationKernel<<<options.islandCount, options.islandPopulationSize * WARP_SIZE>>>(instance, d_globalState, d_population, d_cycleWeight, d_islandBest, d_islandWorst);
 
 		if (cudaDeviceSynchronize() != cudaSuccess)
 			return -1;
 
-		if (cudaMemcpy(h_fitness, d_fitness, islandCount * islandPopulationSize * sizeof(int), cudaMemcpyDeviceToHost) != cudaSuccess)
+		if (cudaMemcpy(h_cycleWeight, d_cycleWeight, options.islandCount * options.islandPopulationSize * sizeof(int), cudaMemcpyDeviceToHost) != cudaSuccess)
 			return -1;
 
-		if (cudaMemcpy(h_islandBest, d_islandBest, islandCount * sizeof(int), cudaMemcpyDeviceToHost) != cudaSuccess)
+		if (cudaMemcpy(h_islandBest, d_islandBest, options.islandCount * sizeof(int), cudaMemcpyDeviceToHost) != cudaSuccess)
 			return -1;
 
-		if (cudaMemcpy(h_islandWorst, d_islandWorst, islandCount * sizeof(int), cudaMemcpyDeviceToHost) != cudaSuccess)
+		if (cudaMemcpy(h_islandWorst, d_islandWorst, options.islandCount * sizeof(int), cudaMemcpyDeviceToHost) != cudaSuccess)
 			return -1;
 
 		std::cout << "Island:\t";
-		for (unsigned int i = 0; i < islandCount; i++)
+		for (unsigned int i = 0; i < options.islandCount; i++)
 			std::cout << i << "\t\t";
 		std::cout << "\nINITIAL\nBest:\t";
-		for (unsigned int i = 0; i < islandCount; i++)
-			std::cout << h_fitness[i * islandPopulationSize + h_islandBest[i]] << "(" << h_islandBest[i] << ")\t";
+		for (unsigned int i = 0; i < options.islandCount; i++)
+			std::cout << h_cycleWeight[i * options.islandPopulationSize + h_islandBest[i]] << "(" << h_islandBest[i] << ")\t";
 		std::cout << "\nWorst:\t";
-		for (unsigned int i = 0; i < islandCount; i++)
-			std::cout << h_fitness[i * islandPopulationSize + h_islandWorst[i]] << "(" << h_islandWorst[i] << ")\t";
+		for (unsigned int i = 0; i < options.islandCount; i++)
+			std::cout << h_cycleWeight[i * options.islandPopulationSize + h_islandWorst[i]] << "(" << h_islandWorst[i] << ")\t";
 
-		for (unsigned int migrationNumber = 1; migrationNumber <= migrationCount; migrationNumber++) {
+		for (unsigned int migrationNumber = 1; migrationNumber <= options.migrationCount; migrationNumber++) {
 
-			migrationKernel<<<islandCount, islandPopulationSize * WARP_SIZE>>>(d_population, nWarpSizeAligned, d_fitness, d_islandBest, d_islandWorst);
+			migrationKernel<<<options.islandCount, options.islandPopulationSize * WARP_SIZE>>>(d_population, nWarpSizeAligned, d_cycleWeight, d_islandBest, d_islandWorst);
 
-			islandEvolutionKernel<<<islandCount, islandPopulationSize * WARP_SIZE>>>(instance, d_globalState, isolatedIterationCount, d_population, d_fitness, d_islandBest, d_islandWorst);
+			islandEvolutionKernel<<<options.islandCount, options.islandPopulationSize * WARP_SIZE>>>(instance, d_globalState, options.isolatedIterationCount, d_population, d_cycleWeight, d_islandBest, d_islandWorst);
 			
 			if (cudaDeviceSynchronize() != cudaSuccess)
 				return -1;
 
-			if (cudaMemcpy(h_fitness, d_fitness, islandCount * islandPopulationSize * sizeof(int), cudaMemcpyDeviceToHost) != cudaSuccess)
+			if (cudaMemcpy(h_cycleWeight, d_cycleWeight, options.islandCount * options.islandPopulationSize * sizeof(int), cudaMemcpyDeviceToHost) != cudaSuccess)
 				return -1;
 
-			if (cudaMemcpy(h_islandBest, d_islandBest, islandCount * sizeof(int), cudaMemcpyDeviceToHost) != cudaSuccess)
+			if (cudaMemcpy(h_islandBest, d_islandBest, options.islandCount * sizeof(int), cudaMemcpyDeviceToHost) != cudaSuccess)
 				return -1;
 
-			if (cudaMemcpy(h_islandWorst, d_islandWorst, islandCount * sizeof(int), cudaMemcpyDeviceToHost) != cudaSuccess)
+			if (cudaMemcpy(h_islandWorst, d_islandWorst, options.islandCount * sizeof(int), cudaMemcpyDeviceToHost) != cudaSuccess)
 				return -1;
 
 			std::cout << "\nCYCLE: " << migrationNumber << "\nBest:\t";
-			for (unsigned int i = 0; i < islandCount; i++)
-				std::cout << h_fitness[i * islandPopulationSize + h_islandBest[i]] << "(" << h_islandBest[i] << ")\t";
+			for (unsigned int i = 0; i < options.islandCount; i++)
+				std::cout << h_cycleWeight[i * options.islandPopulationSize + h_islandBest[i]] << "(" << h_islandBest[i] << ")\t";
 			std::cout << "\nWorst:\t";
-			for (unsigned int i = 0; i < islandCount; i++)
-				std::cout << h_fitness[i * islandPopulationSize + h_islandWorst[i]] << "(" << h_islandWorst[i] << ")\t";
+			for (unsigned int i = 0; i < options.islandCount; i++)
+				std::cout << h_cycleWeight[i * options.islandPopulationSize + h_islandWorst[i]] << "(" << h_islandWorst[i] << ")\t";
 		}
 
-		cudaFree(d_fitness);
+		cudaFree(d_cycleWeight);
 		cudaFree(d_population);
 		cudaFree(d_globalState);
 		cudaFree(d_islandBest);
 		cudaFree(d_islandWorst);
 
-		delete[] h_fitness;
+		delete[] h_cycleWeight;
 		delete[] h_islandBest;
 		delete[] h_islandWorst;
 		
--- main.cpp
+++ main.cpp
@@ -54,7 +54,16 @@ int main(int argc, char *argv[])
     std::cout << "CANONICAL CYCLE TOTAL DISTANCE (device texture memory): " << 
         textureMemoryInstance->hamiltonianCycleWeight(canonicalCycle) << "\n";
 
-    int opt = tsp::solveTSPFineGrained(globalMemoryInstance->deviceInstance(), 4, 10, 10, 100);
+    tsp::IslandGeneticAlgorithmOptions options = {
+        4,      // .islandCount
+        32,     // .islandPopulationSize
+        10,     // .isolatedIterationCount
+        10,     // .migrationCount
+        0.5,    // .crossoverProbability
+		0.5,    // .mutationProbability
+		true    // .elitism
+    };
+    int opt = tsp::solveTSPFineGrained(globalMemoryInstance->deviceInstance(), options, 100);
 
     std::cout << "\n\nOptimal hamiltonian cycle length found: " << opt << "\n";
 
